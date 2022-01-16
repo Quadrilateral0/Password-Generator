89405a7897909d8adc7fde46651cad0724359c1c
Quadrilateral <amandacdulaney@gmail.com>
2022-01-16 18:07:19 -0500

    added js popups to define password parameters

--- assets/script.js
+++ assets/script.js
@@ -26,5 +26,29 @@ generateBtn.addEventListener("click", writePassword);
 // Add event listener to copy button
 copyBtn.addEventListener("click", copyPassword);
 
+// Add popups to define password parameters on click
+document.getElementById("generate").addEventListener("click", function() {
+    var passwordLength = prompt;
+    if (confirm("Would you like to include numbers in your password?\nClick Cancel for no.") == true) {
+        characters.push(0,1,2,3,4,5,6,7,8,9);
+        console.log(characters);
+    } else (console.log(characters));
+    if (confirm("Would you like to include uppercase letters in your password?\nClick Cancel for no.") == true) {
+       characters.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+       console.log(characters);
+    } else (console.log(characters));
+    if (confirm("Would you like to include special characters in your password?\nClick Cancel for no.") == true) {
+        characters.push("!","@","#","$","%","^","&","*","(",")");
+        console.log(characters);
+    } else (console.log(characters));
+    if (prompt("How many total characters would you like in your password?\nClicking Cancel will apply default setting of 8 characters.", "8") == true) {
+        passwordLength = i;
+        console.log(passwordLength);
+    } else if (passwordLength>128 && passwordLength<8) {
+        alert("You entered invalidly. Please enter a number greater than 8 and less than 128.");
+    } else (passwordLength == null); {
+        passwordLength = 8;
+    }   console.log(passwordLength); 
+});
 
 
