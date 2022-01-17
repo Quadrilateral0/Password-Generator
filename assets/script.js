17688bec6c5b923446c2ca4b5f75a3ad626a34c5
Quadrilateral <amandacdulaney@gmail.com>
2022-01-17 15:44:10 -0500

    added return alerts and random number generator

--- assets/script.js
+++ assets/script.js
@@ -1,54 +1,82 @@
 // Define variables
 var generateBtn = document.querySelector("#generate");
 var copyBtn = document.querySelector("#copy");
-var characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var passwordLength = 8;
+var characters = [];
+var password = [];
+var passwordLength = "";
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//Initiate password generator function
+function generatePassword() {
 
-  passwordText.value = password;
+    // Add popups to define password parameters on click
+    document.getElementById("generate").addEventListener("click", function() {
 
-}
+    //Determine password length
+    passwordLength = prompt("Please indicate a password length between 8 and 128.");
+    if (passwordLength >= 8 && passwordLength <= 128) {
+    console.log(passwordLength);
+    } else {
+         return alert("You entered invalidly.\nPlease enter a number of at least 8 but no more than 128.");
+    }   
 
-// Copy password from the #password input
-function copyPassword() {
-    var copyText = document.getElementById("#password");
-    copyText.select();
-    document.execCommand("copy");  
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// Add event listener to copy button
-copyBtn.addEventListener("click", copyPassword);
-
-// Add popups to define password parameters on click
-document.getElementById("generate").addEventListener("click", function() {
-    var passwordLength = prompt;
-    if (confirm("Would you like to include numbers in your password?\nClick Cancel for no.") == true) {
+    //Determine inclusion of numbers in password
+    var charNumber = confirm("Would you like to include numbers in your password?\nClick Cancel for no.");
+    if (charNumber == true) {
         characters.push(0,1,2,3,4,5,6,7,8,9);
         console.log(characters);
+        } else (console.log(characters));
+    
+     //Determine inclusion of lowercase letters in password
+    var charLower = confirm("Would you like to include uppercase letters in your password?\nClick Cancel for no.");
+    if (charLower == true) {
+        characters.push("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+        console.log(characters);
     } else (console.log(characters));
-    if (confirm("Would you like to include uppercase letters in your password?\nClick Cancel for no.") == true) {
-       characters.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
-       console.log(characters);
+
+    //Determine inclusion of uppercase letters in password
+    var charUpper = confirm("Would you like to include uppercase letters in your password?\nClick Cancel for no.");
+    if (charUpper == true) {
+        characters.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+        console.log(characters);
     } else (console.log(characters));
-    if (confirm("Would you like to include special characters in your password?\nClick Cancel for no.") == true) {
+
+    //Determine inclusion of special characters in password
+    var charSymbol = confirm("Would you like to include special characters in your password?\nClick Cancel for no.");
+    if (charSymbol == true) {
         characters.push("!","@","#","$","%","^","&","*","(",")");
         console.log(characters);
     } else (console.log(characters));
-    if (prompt("How many total characters would you like in your password?\nClicking Cancel will apply default setting of 8 characters.", "8") == true) {
-        passwordLength = i;
-        console.log(passwordLength);
-    } else if (passwordLength>128 && passwordLength<8) {
-        alert("You entered invalidly. Please enter a number greater than 8 and less than 128.");
-    } else (passwordLength == null); {
-        passwordLength = 8;
-    }   console.log(passwordLength); 
+
+    //Will return an alert if there no character types are selected
+    if (!charNumber && !charLower && !charUpper && !charSymbol) {
+        return alert('Please select at least one password criteria.');
+    }
+
 });
 
+// Random character generator
+    for (var i = 0; i < passwordLength; i++) {
+        password += characters[Math.floor(Math.random () * characters.length)];
+    } return password;
+}
 
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+    password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
+
+    // Add event listener to copy button
+    //copyBtn.addEventListener("click", copyPassword);
+    // Copy password from the #password input
+  //function copyPassword() {
+    //var copyText = document.getElementById("#password");
+    //copyText.select();
+    //document.execCommand("copy");  
+//} <button id="copy" class="btn">Copy Password</button>
