fcf19c4f29cfa049117bdc94533dc2182b41e0d4
Quadrilateral <amandacdulaney@gmail.com>
2022-01-17 16:21:47 -0500

    removed extraneous console.logs

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,6 @@ function generatePassword() {
     //Determine password length
     passwordLength = prompt("Please indicate a password length between 8 and 128.");
     if (passwordLength >= 8 && passwordLength <= 128) {
-    console.log(passwordLength);
     } else {
          return alert("You entered invalidly.\nPlease enter a number of at least 8 but no more than 128.");
     }   
@@ -21,29 +20,25 @@ function generatePassword() {
     var charNumber = confirm("Would you like to include numbers in your password?\nClick Cancel for no.");
     if (charNumber == true) {
         characters.push(0,1,2,3,4,5,6,7,8,9);
-        console.log(characters);
-        } else (console.log(characters));
+        } else;
     
      //Determine inclusion of lowercase letters in password
     var charLower = confirm("Would you like to include uppercase letters in your password?\nClick Cancel for no.");
     if (charLower == true) {
         characters.push("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
-        console.log(characters);
-    } else (console.log(characters));
+    } else;
 
     //Determine inclusion of uppercase letters in password
     var charUpper = confirm("Would you like to include uppercase letters in your password?\nClick Cancel for no.");
     if (charUpper == true) {
         characters.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
-        console.log(characters);
-    } else (console.log(characters));
+    } else;
 
     //Determine inclusion of special characters in password
     var charSymbol = confirm("Would you like to include special characters in your password?\nClick Cancel for no.");
     if (charSymbol == true) {
         characters.push("!","@","#","$","%","^","&","*","(",")");
-        console.log(characters);
-    } else (console.log(characters));
+    } else;
 
     //Will return an alert if there no character types are selected
     if (!charNumber && !charLower && !charUpper && !charSymbol) {
@@ -66,4 +61,4 @@ function writePassword() {
   
     passwordText.value = password;
   
-  }
+  } 
